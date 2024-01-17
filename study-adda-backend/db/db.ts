dd42ce88a26342127a249a6dacf8abd0a432c78f
yellowhatpro <yellowhatpro3119@gmail.com>
2024-01-17 16:48:32 +0530

    silent migrations made

--- study-adda-backend/db/db.ts
+++ study-adda-backend/db/db.ts
@@ -1,6 +1,7 @@
 import postgres from "postgres";
 import { drizzle } from 'drizzle-orm/postgres-js';
 import dotenv from "dotenv";
+import {migrate} from "drizzle-orm/postgres-js/migrator";
 dotenv.config();
 
 if (!process.env.PSQL_URL){
@@ -8,4 +9,9 @@ if (!process.env.PSQL_URL){
 }
 
 const client = postgres(process.env.PSQL_URL )
+const migrationClient = postgres(process.env.PSQL_URL,{max: 1})
 export const db = drizzle(client)
+await migrate(drizzle(migrationClient), {migrationsFolder: "drizzle"})
+await migrationClient.end()
+
+
---
+++ study-adda-backend/drizzle.config.ts
@@ -0,0 +1,12 @@
+import 'dotenv/config';
+import type { Config } from 'drizzle-kit';
+import dotenv from "dotenv";
+dotenv.config()
+export default {
+    schema: './db/schema/*',
+    out: './drizzle',
+    driver: 'pg', // 'pg' | 'mysql2' | 'better-sqlite' | 'libsql' | 'turso'
+    dbCredentials: {
+        connectionString: process.env.PSQL_URL
+    },
+} satisfies Config;
