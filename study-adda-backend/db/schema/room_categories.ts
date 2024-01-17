e3f2a8464f243b623772156cd10f923a95f786aa
yellowhatpro <yellowhatpro3119@gmail.com>
2024-01-17 13:38:15 +0530

    added room schema, room category m2m relation

---
+++ study-adda-backend/db/schema/room_categories.ts
@@ -0,0 +1,8 @@
+import {pgTable, serial, varchar} from "drizzle-orm/pg-core/index";
+
+export const roomCategories = pgTable(
+    'room_categories', {
+        id: serial('id').primaryKey(),
+        name: varchar('name', {length: 256})
+    }
+)
---
+++ study-adda-backend/db/schema/rooms.ts
@@ -0,0 +1,19 @@
+import {pgTable, serial, varchar} from "drizzle-orm/pg-core";
+import {roomCategories} from "./room_categories";
+
+export const rooms = pgTable(
+    'rooms', {
+        id: serial('id').primaryKey(),
+        name: varchar('name', {length: 256})
+    }
+)
+
+//Keeping Room to Category Relation, as I want rooms to belong to >=1 categories
+//Also I want categories to show rooms of such types
+
+export const roomsToRoomCategories = pgTable(
+    'rooms_to_rooms_categories', {
+        roomId: serial('user_id').notNull().references(()=>rooms.id),
+        categoryId: serial('category_id').notNull().references(()=>roomCategories.id)
+    }
+)
