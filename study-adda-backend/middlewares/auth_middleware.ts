4a7ed233d00a11aa19efd4de0388dad4873bb2fa
yellowhatpro <yellowhatpro3119@gmail.com>
2024-01-19 07:07:28 +0530

    protected route added

---
+++ study-adda-backend/middlewares/auth_middleware.ts
@@ -0,0 +1,11 @@
+import JWT from "jsonwebtoken";
+
+//Protected Route token based
+export const requireSignIn = async(req, res, next) => {
+    try {
+        req.user = JWT.verify(req.headers.authorization, process.env.JWT_SECRET)
+        next()
+    } catch (e) {
+        console.log(e)
+    }
+}
--- study-adda-backend/src/routes/auth_routes.ts
+++ study-adda-backend/src/routes/auth_routes.ts
@@ -1,8 +1,12 @@
 import express from "express";
-import {registerController} from "../controllers/auth_controllers";
+import {loginController, registerController} from "../controllers/auth_controllers";
+import {requireSignIn} from "../../middlewares/auth_middleware";
 
 const router = express.Router()
 
 router.post('/register', registerController)
+router.post('/login', loginController)
+router.get('/user-auth', requireSignIn, (req, res)=>res.status(200).send({ok: true}))
+
 
 export default router
