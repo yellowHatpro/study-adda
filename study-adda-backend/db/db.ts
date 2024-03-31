57913276cc73a7cd0216924d5dc2bde33598e446
yellowhatpro <yellowhatpro3119@gmail.com>
2024-03-31 14:49:15 +0530

    prod getting ready

--- study-adda-backend/db/db.ts
+++ study-adda-backend/db/db.ts
@@ -7,8 +7,11 @@ dotenv.config();
 if (!process.env.PSQL_URL) {
     throw new Error("DB credentials error");
 }
-
-const client = postgres(process.env.PSQL_URL);
+const client = postgres(
+    process.env.NODE === "development"
+        ? process.env.PSQL_URL_DEV
+        : process.env.PSQL_URL_PROD,
+);
 export const db = drizzle(client, {
     schema: schema,
 });
