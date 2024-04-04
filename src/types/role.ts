1f984c827e7d38b7ec0c87090bc70ced39136e71
yellowhatpro <yellowhatpro3119@gmail.com>
2024-04-04 10:11:45 +0530

    revert prod for now

--- src/types/role.ts
+++ src/types/role.ts
@@ -1,4 +1,4 @@
 export enum Role {
     ADMIN,
-    USER
+    USER,
 }
--- study-adda-backend/db/db.ts
+++ study-adda-backend/db/db.ts
@@ -2,16 +2,13 @@ import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
 import dotenv from "dotenv";
 import * as schema from "./index";
+import * as process from "process";
 dotenv.config();
 
-if (!process.env.PSQL_URL) {
+if (!process.env.PSQL_URL_DEV) {
     throw new Error("DB credentials error");
 }
-const client = postgres(
-    process.env.NODE === "development"
-        ? process.env.PSQL_URL_DEV
-        : process.env.PSQL_URL_PROD,
-);
+const client = postgres(process.env.PSQL_URL_DEV);
 export const db = drizzle(client, {
     schema: schema,
 });
