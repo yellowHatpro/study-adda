5388e0ae0eed68d5468e0fd57ff1520e21696c03
yellowhatpro <yellowhatpro3119@gmail.com>
2024-04-20 10:15:05 +0530

    URL fixes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const URL = `${(import.meta.env.VITE_NODE_ENV==="development") ? "http://localhost:8080": ""}`
+export const URL = import.meta.env.VITE_HOST
 export const LOCAL_STORAGE_ACCESS_TOKEN = "lc-access-token"
 export const USER_NO_ROOM_CREATED = "user has not created any room yet"
 export const NETWORK_ISSUE = "network issue"
