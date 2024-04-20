474991720216523b3e7458b1604bf6e0d434fd5d
yellowhatpro <yellowhatpro3119@gmail.com>
2024-04-20 08:45:37 +0530

    workflow fixes:

--- study-adda-backend/middlewares/auth_middleware.ts
+++ study-adda-backend/middlewares/auth_middleware.ts
@@ -2,23 +2,24 @@ import JWT from "jsonwebtoken";
 
 //Protected Route token based
 export const requireSignIn = async (req, res, next) => {
-    try {
-        const authHeader = req.headers["authorization"];
-        const token = authHeader && authHeader.split(" ")[1];
-        if (!token) return res.status(401).send({
-            success: false,
-            message: "Token not found"
+  try {
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    if (!token)
+      return res.status(401).send({
+        success: false,
+        message: "Token not found",
+      });
+    JWT.verify(token, process.env.JWT_SECRET, (err, user) => {
+      if (err)
+        return res.status(403).send({
+          success: false,
+          message: "Error verifying using JWT",
         });
-        JWT.verify(token, process.env.JWT_SECRET, (err, user) => {
-            if (err) return res.status(403).send({
-                success: false,
-                message: "Error verifying using JWT"
-            });
-            req.user = user;
-            next();
-        });
-
-    } catch (e) {
-        console.log("Auth Middleware error", e);
-    }
-}
+      req.user = user;
+      next();
+    });
+  } catch (e) {
+    console.log("Auth Middleware error", e);
+  }
+};
